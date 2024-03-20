42ada4e9ccf46d02fd6c46d7649a487c39ba8756
kanchan <kanchankotkar001@gmail.com>
2024-03-20 22:40:50 +0530

    initial commit

---
+++ contactus.php
@@ -0,0 +1,22 @@
+ <!-- start contacr us -->
+ <div class="container mt-4" id="contact">
+    <h2 class="text-center mb-4">Contact Us</h2>
+    <div class="row">
+      <div class="col-md-8">
+        <form action="" method="post">
+          <input type="text" class="form-control" name="name" placeholder="Name"></br>
+          <input type="text" class="form-control" name="subject" placeholder="Subject"></br>
+          <input type="text" class="form-control" name="email" placeholder="E-mail"></br>
+          <textarea class="form-control" name="message" placeholder="how we can help you ?"
+            style="height: 150px; "></textarea></br>
+          <input class="btn btn-primary" type="submit" value="send" name="submit"><br><br>
+        </form>
+      </div>
+
+      <div class="col-md-4 stripe text-white text-center">
+        <h4>Ischool</h4>
+        <p>ischool hfoildlksahf jhfdlkas 235457lkj oihfidskmckjdsc oijisd;lm 65r32jfj;lsfmpggonjbon</p>
+      </div>
+    </div>
+  </div>
+  <!-- end contact us -->
